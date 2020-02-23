b71e004de8374b42b4ab904db3685b62baacbdad
Ben Clayton-Green <ben-clayton-green@Bens-MacBook-Air.local>
2020-02-23 10:32:14 +0000

    rearrangement

--- script.js
+++ script.js
@@ -1,58 +1,56 @@
-let ballPos = {
-    x:0,
-    y:0
-}
+let xSpeed = 10
+let ySpeed = 15
+const xAccelaration = 0.833
+const yAccelaration = 3.27
+const rippleRate = 3 // lower means more frequent
 
+let ballPos = {x:0, y:0}
 const ball = document.getElementById('ball')
 const main = document.getElementById("main")
+let createRipple = 0
 
-let createRipple = true
 
-begin()
+begin();
 
 function begin () {
     setInterval(()=>{
-        calculateBallPos()
-        setBallPos(ball)
-        if (createRipple){
-            createTempBall('ball1')
-            createTempBall('ball2')
-            createTempBall('ball3')
-            createTempBall('ball4')
+        moveBall()
+        setPos(ball)
+        if (createRipple === 0){
+            createTempDiv('ripple-1')
+            createTempDiv('ripple-2')
+            createTempDiv('ripple-3')
+            createTempDiv('ripple-4')
+            createRipple = rippleRate
         }
-        createRipple = !createRipple
+        createRipple --
     },200)
 } 
 
-function setBallPos(div) {
+function moveBall() {
+    xSpeed -= ballPos.x * xAccelaration/10
+    ballPos.x += xSpeed
+    ySpeed -= ballPos.y * yAccelaration/10
+    ballPos.y += ySpeed
+}
+
+function setPos(div) {
     div.style.cssText = `
             left: ${ballPos.x + 50}%;
             top: ${ballPos.y + 50}%;
         `
 }
 
-let xSpeed = 10
-let ySpeed = 15
-
-function calculateBallPos() {
-    xSpeed -= ballPos.x/12
-    ballPos.x += xSpeed
-    ySpeed -= ballPos.y/3.06
-    ballPos.y += ySpeed
-}
-
-function createTempBall(ballType) {
+function createTempDiv(divType) {
     const extraBall = document.createElement('div')
-    setBallPos(extraBall)
+    setPos(extraBall)
     setTimeout(() => {
         main.appendChild(extraBall)
         setTimeout(() => {
-            extraBall.classList.add(ballType)
+            extraBall.classList.add(divType)
         },10)
         setTimeout(() => {
             main.removeChild(extraBall);
         },1600)
     }, 220)
 }
-
-
